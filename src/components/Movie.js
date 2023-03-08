28628c0c0a4db97a1953f798ea348bfd5629ffe0
APSDIGIT\vballut <vincentballut@outlook.fr>
2023-03-08 16:29:25 +0100

    Add breadcrumb

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,8 @@ import { useMovieFetch } from '../hooks/useMovieFetch';
 //config
 
 //Components
+import Spinner from './Spinner'
+import BreadCrumb from './BreadCrumb';
 
 //Image
 
@@ -13,10 +15,17 @@ import { useMovieFetch } from '../hooks/useMovieFetch';
 const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
-  console.log({ movie });
+  if (loading){
+    return(<Spinner />)
+  }
+
+  if (error){
+    return (<div>Something went wrong ...</div>)
+  }
+
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
   );
 };
