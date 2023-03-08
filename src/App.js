92be724327824eb203fa719d5f55b4482af5e8e6
APSDIGIT\vballut <vincentballut@outlook.fr>
2023-03-08 15:16:56 +0100

    add some routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 import { GlobalStyle } from './GlobalStyle';
-import HeroImage from './components/HeroImage';
 
+//routing
 //Components
 //Styles
 //Styles
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
+const App = () => (
+  <Router className="App">
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:movieId" element={<Movie />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
 
-      <GlobalStyle />
-    </div>
-  );
-};
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
